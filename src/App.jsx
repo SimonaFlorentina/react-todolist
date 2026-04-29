76c7ac11c8a16e0a9630d6f0bc4a19da58954a8f
Simona Birligea <simonabirligea@192.168.1.142>
2026-04-29 17:14:56 +0300

    Add missing useRef import and fix React build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { ref, onValue, set } from 'firebase/database'
 import { db } from './firebase'
 
